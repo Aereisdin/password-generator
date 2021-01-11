59385f57147a1fbb7b5b391c266c5df3d21ee52b
Kiel Sprague <aereisdin@gmail.com>
2021-01-11 13:57:13 -0800

    Script changes

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"];
 
-var capLet = capLetter[Math.floor(Math.random() * capLetter.length)];
-var lowLet = lowLetter[Math.floor(Math.random() * lowLetter.length)];
-var num = number[Math.floor(Math.random() * number.length)];
-var spec = special[Math.floor(Math.random() * special.length)];
+// var capLet = capLetter[Math.floor(Math.random() * capLetter.length)];
+// var lowLet = lowLetter[Math.floor(Math.random() * lowLetter.length)];
+// var num = number[Math.floor(Math.random() * number.length)];
+// var spec = special[Math.floor(Math.random() * special.length)];
 
 // var capLet = capLetter.charAt(Math.floor(Math.random() * capLetter.length));
 
@@ -25,24 +25,24 @@ var spec = special[Math.floor(Math.random() * special.length)];
 
 function button(){
         var length = prompt("How long would you like your passord to be? Please enter a number between 8 and 128.");{}
-            if(length > 8){
+            if(length < 8){
                 prompt("Please select a higher number.")
             }
-            else if(length < 128){
+            else if(length > 128){
                 prompt("Please select a lower number.")
             }
-        var choice = prompt("Please choose the next character in your new password. 'c' for a capital letter, 'l' for a lowercase letter, 'n' for a number or 's' for a special character.");{
-            if(choice === "c".toLowerCase){
-            document.getElementById("password").innerHTML = capLet;
+        var choice = prompt("Please choose the next character in your new password. c for a capital letter, l for a lowercase letter, n for a number or s for a special character.");{
+            if(choice === "c"){
+            document.getElementById("password").innerHTML = capLetter[Math.floor(Math.random() * capLetter.length)];
              }
-             else if(choice === "l".toLowerCase){
-            document.getElementById("password").innerHTML = lowLet;
+             else if(choice === "l"){
+            document.getElementById("password").innerHTML = lowLetter[Math.floor(Math.random() * lowLetter.length)];
              }
-             else if(choice === "n".toLowerCase){
-            document.getElementById("password").innerHTML = num;
+             else if(choice === "n"){
+            document.getElementById("password").innerHTML = number[Math.floor(Math.random() * number.length)];
              }
-             else if(choice === "s".toLowerCase){
-            document.getElementById("password").innerHTML = spec;
+             else if(choice === "s"){
+            document.getElementById("password").innerHTML = special[Math.floor(Math.random() * special.length)];
             }
             else prompt("That is not a valid choice") 
     }for(let i = 0; i < length; i--){
