ca6bebfa72bc94f370abbb65065abea3624d2638
Kiel Sprague <aereisdin@gmail.com>
2021-01-11 14:59:33 -0800

    Got the loop to work. Now it will not print to h2 element with every loop

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ var special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "
 // var num = number[Math.floor(Math.random() * number.length)];
 // var spec = special[Math.floor(Math.random() * special.length)];
 
+// console.log(capLet)
+// console.log(lowLet)
+
 // var capLet = capLetter.charAt(Math.floor(Math.random() * capLetter.length));
 
 // var lowLet = lowLetter.charAt(Math.floor(Math.random() * lowLetter.length));
@@ -24,29 +27,37 @@ var special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "
 // var spec = special.charAt(Math.floor(Math.random() * special.length));
 
 function button(){
-        var length = prompt("How long would you like your passord to be? Please enter a number between 8 and 128.");{}
+        var length = prompt("How long would you like your passord to be? Please enter a number between 8 and 128.");{
             if(length < 8){
-                prompt("Please select a higher number.")
+                prompt("Please select a higher number.");
             }
             else if(length > 128){
-                prompt("Please select a lower number.")
+                prompt("Please select a lower number.");
             }
+        }
+        
+        var capLet = capLetter[Math.floor(Math.random() * capLetter.length)];
+        var lowLet = lowLetter[Math.floor(Math.random() * lowLetter.length)];
+        var num = number[Math.floor(Math.random() * number.length)];
+        var spec = special[Math.floor(Math.random() * special.length)];
+        for(var i = 0; i < length; i++){
         var choice = prompt("Please choose the next character in your new password. c for a capital letter, l for a lowercase letter, n for a number or s for a special character.");{
             if(choice === "c"){
-            document.getElementById("password").innerHTML = capLetter[Math.floor(Math.random() * capLetter.length)];
+            document.getElementById("password").innerHTML = capLet;
              }
              else if(choice === "l"){
-            document.getElementById("password").innerHTML = lowLetter[Math.floor(Math.random() * lowLetter.length)];
+            document.getElementById("password").innerHTML = lowLet;
              }
              else if(choice === "n"){
-            document.getElementById("password").innerHTML = number[Math.floor(Math.random() * number.length)];
+            document.getElementById("password").innerHTML = num;
              }
              else if(choice === "s"){
-            document.getElementById("password").innerHTML = special[Math.floor(Math.random() * special.length)];
+            document.getElementById("password").innerHTML = spec;
             }
             else prompt("That is not a valid choice") 
-    }for(let i = 0; i < length; i--){
+        }
+    }
+        
+}
     
-}
-}
 
