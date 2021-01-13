678f77bc676ca5128036002a31e9c370061be4f5
Kiel Sprague <aereisdin@gmail.com>
2021-01-13 12:25:31 -0800

    I've created a different version.

--- script.js
+++ script.js
@@ -27,22 +27,22 @@ function button(){
         var num = number[Math.floor(Math.random() * number.length)];
         var spec = special[Math.floor(Math.random() * special.length)];
 
-        var character = [lowLet,]
+        var character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
         var capital = confirm("Would you like capital letters in your password?");{
-            if(capital === true){character.push(capLet); 
+            if(capital === true){character.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"); 
                 alert("You have selected to add capital letters to your password.");}
             else alert("You will not have capital letters in your password.");
             }
 
         var numbers = confirm("Would you like numbers in your password?");{
-            if(numbers === true){character.push(num);
+            if(numbers === true){character.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
             alert("You have selected to add numbers to your password.");}
             else alert("You will not have numbers in your password.");
             }
 
         var specials = confirm("Would you like special characters in your password?");{
-            if(specials === true){character.push(spec);
+            if(specials === true){character.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+");
             alert("You have selected to add special characters to your password.")}
             else alert("You will not have numbers in your password.")
              }
